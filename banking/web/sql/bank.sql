ca3dcc194e5c182242c8c97d59f9ffa0307a8a9d
wch960426@126.com <wch960426@126.com>
2017-07-11 22:34:41 +0800

    commit SQL

---
+++ banking/web/sql/bank.sql
@@ -0,0 +1,23 @@
+CREATE database banking;
+
+use bbs;
+
+CREATE TABLE customer(
+id INT PRIMARY KEY auto_increment,
+name VARCHAR (255),
+bank VARCHAR (255)
+);
+
+CREATE TABLE  checkingaccount(
+id INT PRIMARY KEY auto_increment,
+pid INT ,
+balance DOUBLE ,
+protect DOUBLE
+);
+
+CREATE TABLE savingaccount(
+id INT PRIMARY KEY auto_increment,
+pid INT ,
+balance DOUBLE ,
+interesRate DOUBLE
+);
